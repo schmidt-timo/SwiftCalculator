dfa6fc5ea78536df45ba17e47b866140fcfee8ee
Timo Schmidt <schmidt@timo.email>
2020-08-01 23:27:46 +0200

    Initial Commit

---
+++ SwiftCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by Timo Schmidt on 01.08.20.
+//  Copyright © 2020 Timo Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
